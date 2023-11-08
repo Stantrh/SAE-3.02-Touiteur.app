50b10b7d889a4d49d00157da71ad638fd94715bd
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-11-08 19:04:21 +0100

    l'upload de fichier fonctionne parfaitement, reste plus qu'à modif le renderer de touite

---
+++ src/upload.php
@@ -0,0 +1,24 @@
+<?php
+require '../vendor/autoload.php';
+
+use Upload\Storage\FileSystem;
+use Upload\File;
+use Upload\Validation\Mimetype;
+use Upload\Validation\Size;
+
+$storage = new FileSystem('../images'); // Répertoire où les fichiers seront stockés
+$file = new File('image', $storage);
+
+// Les types MIME autorisés
+$file->addValidations([
+    new Mimetype(['image/jpeg', 'image/png', 'image/jpg']),
+    new Size('5M') // Limite la taille du fichier à 5 Mo
+]);
+
+// Essayez d'uploader le fichier
+try {
+    $file->upload();
+    echo 'Le fichier a été téléchargé avec succès!';
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,30 +25,11 @@ class ComposerAutoloaderInitbe1986a4911153a91e3e0397a3b1fd5d
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitbe1986a4911153a91e3e0397a3b1fd5d', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitbe1986a4911153a91e3e0397a3b1fd5d', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInitbe1986a4911153a91e3e0397a3b1fd5d::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInitbe1986a4911153a91e3e0397a3b1fd5d::getInitializer($loader));
 
         $loader->register(true);
 
