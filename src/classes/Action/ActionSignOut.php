a02815396b0eedd89ab41b81b8b72b59ffd5d0c6
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-11-09 19:30:41 +0100

    petite modif sur le signout mais tout marche parfait

--- src/classes/Action/ActionSignOut.php
+++ src/classes/Action/ActionSignOut.php
@@ -4,15 +4,14 @@ namespace touiteur\Action;
 
 class ActionSignOut extends Action
 {
-
-
     public function execute(): string
     {
         $user = unserialize($_SESSION['user']);
         $nomUser = $user->__get('nomUser');
         session_destroy();
-        return "<h4>Vous avez bien été déconnecté $nomUser !</h4>";
 
-
+        // Redirection vers la page d'accueil avec l'action par défaut
+        header('Location: ?action=default');  // Assure-toi que c'est le chemin correct vers ta page d'accueil
+        exit;
     }
 }
